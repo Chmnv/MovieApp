56bea73a55dd28816fc840f1d642bdc4974c5a49
vakho <v_cheminava@cu.edu.ge>
2025-06-06 00:16:16 +0400

    Modal Window

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const API_KEY = "028f8a1c-bb5f-4986-a2c4-f22e5eca559c";
 const API_URL_POPULAR =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL"
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL";
 const API_URL_SEARCH =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_MOVIE_DETAILS =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 
 getMovies(API_URL_POPULAR);
 
@@ -35,36 +37,27 @@ function showMovies(data) {
   const movies = data.items || data.films || [];
 
   movies.forEach((movie) => {
-    const rating =
-      movie.rating || movie.ratingKinopoisk || movie.ratingImdb || null;
+    const rating = movie.rating || movie.ratingKinopoisk || movie.ratingImdb || null;
+    const title = movie.nameOriginal || movie.nameEn || movie.nameRu;
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
+    movieEl.dataset.id = movie.kinopoiskId || movie.filmId;
+
     movieEl.innerHTML = `
       <div class="movie__cover-inner">
-        <img
-          src="${movie.posterUrlPreview}"
-          class="movie__cover"
-          alt="${movie.nameRu || movie.nameEn}"
-        />
+        <img src="${movie.posterUrlPreview}" class="movie__cover"alt="${title}"/>
         <div class="movie__cover--darkened"></div>
       </div>
+
       <div class="movie__info">
-        <div class="movie__title">${movie.nameRu || movie.nameEn}</div>
-        <div class="movie__category">${movie.genres?.map(
-      (genre) => ` ${genre.genre}`
-    ).join(", ") || ''}</div>
-        ${rating
-        ? `<div class="movie__average movie__average--${getClassByRate(
-          rating
-        )}">${rating}</div>`
-        : ""}
-      </div>
-    `;
+        <div class="movie__title">${title}</div>
+        <div class="movie__category">${movie.genres?.map((genre) => genre.genre).join(", ") || ""}</div>
+        ${rating ? `<div class="movie__average movie__average--${getClassByRate(rating)}">${rating}</div>` : ""}
+      </div>`;
     moviesEl.appendChild(movieEl);
   });
 }
-
 
 const form = document.querySelector("form");
 const search = document.querySelector(".header__search");
@@ -75,7 +68,70 @@ form.addEventListener("submit", (e) => {
   const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
   if (search.value) {
     getMovies(apiSearchUrl);
-
     search.value = "";
+  }
+});
+
+// Modal Window
+const modalEl = document.querySelector(".modal");
+
+async function openModal(id) {
+  const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+
+  const title = respData.nameOriginal || respData.nameEn || respData.nameRu;
+
+
+  modalEl.classList.add("modal--show");
+  document.body.classList.add("stop-scrolling");
+
+  modalEl.innerHTML = `
+    <div class="modal__card">
+      <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
+      <h2>
+        <span class="modal__movie-title">${title}</span>
+        <span class="modal__movie-release-year"> - ${respData.year}</span>
+      </h2>
+
+      <ul class="modal__movie-info">
+        <li class="modal__movie-genre">Genre - ${respData.genres.map((el) => `<span>${el.genre}</span>`).join(", ")}</li>
+        ${respData.filmLength ? `<li class="modal__movie-runtime">Time - ${respData.filmLength} min</li>` : ""}
+        <li>Site: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
+        <li class="modal__movie-overview">Description - ${respData.description}</li>
+      </ul>
+      <button type="button" class="modal__button-close">Close</button>
+    </div>
+  `;
+
+  const btnClose = document.querySelector(".modal__button-close");
+  btnClose.addEventListener("click", () => closeModal());
+}
+
+function closeModal() {
+  modalEl.classList.remove("modal--show");
+  document.body.classList.remove("stop-scrolling");
+}
+
+window.addEventListener("click", (e) => {
+  if (e.target === modalEl) {
+    closeModal();
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+});
+
+document.querySelector(".movies").addEventListener("click", (e) => {
+  const movieEl = e.target.closest(".movie");
+  if (movieEl && movieEl.dataset.id) {
+    openModal(movieEl.dataset.id);
   }
 });
